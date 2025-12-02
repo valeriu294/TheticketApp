586e8614e9921fe65dd4d25f0e3d4759826d12c9
Valeriu Coman <0comav27@solent.ac.uk>
2025-12-02 15:31:14 +0000

    add class

--- src/models/destination.kt
+++ src/models/destination.kt
@@ -1,11 +1,10 @@
 package models
 
-class destination(
-    var name: String = "",
-    var singlePrice: Double = 0.0,
-    var returnPrice: Double = 0.0,
-    var salesCount: Int = 0,
-    val activeOffers: MutableList<SpecialOffer> = mutableListOf()
-){
-
+data class Destination(
+    var name: String,           // Station name
+    var singlePrice: Double,     // Price for single ticket
+    var returnPrice: Double,     // Price for return ticket
+    var ticketsSold: Int = 0,    // Number of tickets sold to this destination
+    var takings: Double = 0.0    // Total revenue from this destination
+)
 }
